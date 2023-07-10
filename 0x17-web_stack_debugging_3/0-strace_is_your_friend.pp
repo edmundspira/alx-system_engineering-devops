b7b9b4c5dfd3c16e2151c30a0db613aab4eaab41
Edmund Spira <109545919+edmundspira@users.noreply.github.com>
2023-07-10 09:38:02 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Puppet code to change ".phpp" to ".php" using the SED command
-
-exec { 'type fixer ftw':
-    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
-    provider => shell,
-}
